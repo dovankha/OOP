a864cae43bf95ce778f2320eda53c3a58a07814b
dovankha <dovankha.tlvn@gmail.com>
2020-08-31 16:22:02 +0700

    first commit

---
+++ TinhChatDongGoi_Encapsulation.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class sinhVien
+{
+    // thuoc tinh
+private:
+    string ten, MSSV;
+    float diemToan, diemLy;
+    // phuong thuc
+public:
+    void nhapThongTin();
+    void xuatThongtin();
+
+    // co che dong goi getter & setter
+
+    string getTen()
+    {
+        return ten;
+    }
+
+    void SetTen(string TEN)
+    {
+        ten = TEN;
+    }
+    // ham tao va ham huy
+
+    sinhVien();
+    ~sinhVien();
+};
+
+int main()
+{
+    sinhVien kha;
+    cout << "========= NHAP THONG TIN ==========" << endl;
+    kha.nhapThongTin();
+    cout << "\n========= XUAT THONG TIN ==========" << endl;
+    kha.xuatThongtin();
+    system("pause");
+    return 0;
+}
+
+void sinhVien::nhapThongTin()
+{
+    cout << "\nNhap Ho & Ten: ";
+    getline(cin, ten);
+    cout << "\nNhap MSSV: ";
+    getline(cin, MSSV);
+    cout << "\nNhap diem toan: ";
+    cin >> diemToan;
+    cout << "\nNhap diem ly: ";
+    cin >> diemLy;
+}
+
+void sinhVien::xuatThongtin()
+{
+    cout << "\nHo & Ten: " << ten << endl;
+    cout << "MSSV: " << MSSV << endl;
+    cout << "Diem Toan: " << diemToan << endl;
+    cout << "Diem ly: " << diemLy << endl;
+}
+
+sinhVien::sinhVien()
+{
+}
+
+sinhVien::~sinhVien()
+{
+}
